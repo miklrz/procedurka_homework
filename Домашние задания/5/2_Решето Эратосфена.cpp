ac8184b258e40727a5961fce0eb60fc4eb3d3961
miklrz <arz.mikhail@gmail.com>
2023-12-07 18:51:32 +0300

    5.2 делаю

--- Домашние задания/5/2_Решето Эратосфена.cpp
+++ Домашние задания/5/2_Решето Эратосфена.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+	int arr[1000];
+	arr[0] = true;
+	arr[1] = true;
+	arr[2] = true;
+	int n;
+	cout << "¬ведите число :"; if (!(cin >> n)) { "¬ведите корректные данные!"; }
+	for (int i = 3; i < n; ++i) {
+		bool _ = false;
+		for (int j = 1; j < i; j++) {
+			if (i % j == 0 and i != j) {
+				arr[i] = false;
+				_ = true;
+				break;
+			}
+			
+		}
+		if (_ == false) { arr[i] = true; }
+	}
+	for (int i = 0; i < n; ++i) {
 
+		cout <<i << " "<< arr[i] << "\n";
+	}
 }
